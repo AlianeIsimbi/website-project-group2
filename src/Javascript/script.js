711f63886c05bcfd461b00ff258d75461244b125
AlianeIsimbi <isimbialiane@gmail.com>
2025-08-29 11:16:27 +0200

    Final touches

--- src/Javascript/script.js
+++ src/Javascript/script.js
@@ -1,25 +1,20 @@
 const hamburger = document.getElementById('hamburger')
         const mobileMenu = document.getElementById('mobileMenu')
-        const line1 = document.getElementById('line1')
-        const line2 = document.getElementById('line2')
-        const line3 = document.getElementById('line3')
+        const line1 = document.getElementById('burger')
+        const line2 = document.getElementById('con')
         
         let isOpen = false;
 
         hamburger.addEventListener('click', () =>{
-            if(!isOpen){
-                mobileMenu.classList.remove('hidden')
-                line1.style.transform = 'rotate(45deg) translate(4px, 4px)';
-                line2.style.opacity = '0';
-                line3.style.transform = 'rotate(-45deg) translate(4px, -4px)';
-
-                isOpen = true;
-            }else{
+            if(isOpen){
                 mobileMenu.classList.add('hidden')
-                line1.style.transform = 'rotate(0) translate(0px, 0px)';
-                line2.style.opacity = '1';
-                line3.style.transform = 'rotate(0) translate(0px, 0px)';
-
+                line1.classList.remove('hidden')
+                line2.classList.add('hidden')
                 isOpen = false;
+            }else{
+                mobileMenu.classList.remove('hidden')
+                line1.classList.add('hidden')
+                line2.classList.remove('hidden')
+                isOpen = true;
             }
         })
